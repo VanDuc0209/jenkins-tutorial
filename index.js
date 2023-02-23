356e0788353dfc988e10eda4e639c388ca87dafa
vanduc0209 <caovanduca2@gmail.com>
2023-02-23 21:53:20 +0700

    fix: message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello, world')
+    res.send('Hello World!!!')
 });
 app.listen(PORT, () => {
     console.log(`App listening on http://127.0.0.1:3000`);
